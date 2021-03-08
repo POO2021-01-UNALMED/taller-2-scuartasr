8bc3618c0ae41bf164712ac5798aee2e717ebe9c
Simón Cuartas <scuartasr@unal.edu.co>
2021-03-08 10:48:54 -0500

    Intento 3
    xd

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -19,23 +19,26 @@ public class Auto {
 		return contador;
 	}
 	public String verificarIntegridad() {
+		// Tomar únicamente los elementos no-null de Asientos en la lista sillas
 		List<Asiento> sillas = new ArrayList<Asiento>();
 		for(Asiento sillita: asientos) {
 			if (sillita != null) {
 				sillas.add(sillita);
 			}
 		}
-		boolean integro = true;
-		if (sillas.get(0).registro != motor.registro || motor.registro != this.registro) {
-			integro = false;
+		// Tomar los registros de las diferentes sillas.
+		int[] registros = new int[sillas.size()];
+		for(int j = 0; j < sillas.size(); j++) {
+			registros[j] = sillas.get(j).registro;
 		}
-		for (int i = 0; i < (sillas.size()-1); i++) {
-			if (sillas.get(i) != sillas.get(i+1)) {
-				integro = false;
-				break;
+		// Definición de un booleando y chequeo de los registros
+		boolean integro = false;		
+		for (int i = 1; i < registros.length; i++) {
+			if (registros[i] == registros[0] && registros[0] == motor.registro && this.registro == motor.registro) {
+				integro = true;
 			}
 		}
-		if (integro) {
+		if (!integro) {
 			return "Auto original";
 		}
 		else {
